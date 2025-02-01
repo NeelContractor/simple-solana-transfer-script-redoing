d9838912143df7d847a027e790c12f1a8a0315ca
Neel Contractor <neelcontractor@Neels-MacBook-Air.local>
2025-02-01 16:24:06 +0530

    added script to create a account on chain with some byte space allcoated

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
-import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js";
+import { Connection, Keypair, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 (async() => {
+    // this script create a account with some byte space allocated 
     
     const connection = new Connection(process.env.RPC!);
+
+    const dataAccount = Keypair.generate();
+    console.log("Data Account: " + dataAccount.publicKey.toBase58());
     
     const payer = Keypair.fromSecretKey(Uint8Array.from(JSON.parse(process.env.PAYER!)));
-    console.log("Payer's Address:" + payer.publicKey.toBase58());
+    console.log("Payer's Address: " + payer.publicKey.toBase58());
 
-    const lamports = 1 * LAMPORTS_PER_SOL;
-    
-    const recevier = Keypair.generate();
-    console.log("recevier : " + recevier.publicKey.toBase58());
-    
-    const transaction = new Transaction();
-    
-    const ix = SystemProgram.transfer({
-        fromPubkey: payer.publicKey,
-        toPubkey: recevier.publicKey,
-        lamports
-    });
+    console.log("hi there1")
+    const tx = new Transaction().add(
+        SystemProgram.createAccount({
+            fromPubkey: payer.publicKey,
+            newAccountPubkey: dataAccount.publicKey,
+            lamports: await connection.getMinimumBalanceForRentExemption(1000),
+            space: 1000,
+            programId: SystemProgram.programId
+        })
+    )
 
-    const tx = transaction.add(ix);
+    console.log("hi there")
+    const txId = await connection.sendTransaction(tx, [payer, dataAccount]);
 
-    const sign = await connection.sendTransaction(tx, [payer]);
-
-    console.log("Signature : " + sign);
+    console.log(`Created account with transaction ID: ${txId}`);
 
 })();
